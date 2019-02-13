a95c4b60d7bde4af1d1caf06ba65c3bdeca9c7d4
Joshua <joshua@Joshua.local>
2019-02-13 12:28:00 -0600

    Cleaning up the code

--- jzip.c
+++ jzip.c
@@ -1,29 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+FILE *getFilePtr(char*);
+long getFileLength(FILE*);
+void createBinaryArray(FILE*, long, int binaryArray[]);
+
 void charToBinary(int arr[], char);
+void binaryToChar(int arr[], int, int);
 int compressArray(int binaryArray[], int newArray[], int arrLen);
 void printBinArray(int arr[], int);
 
-FILE *fileptr;
-char *buffer;
-long filelen;
 
 FILE *writeptr;
 char *newBuffer;
 
 int main (int argc, char **argv) {
-	fileptr = fopen(argv[1], "rb"); 	 // Open the file in binary mode
-	fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
-	filelen = ftell(fileptr);             // Get the current byte offset in the file
-	rewind(fileptr);                      // Jump back to the beginning of the file
-	buffer = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
-	fread(buffer, filelen, 1, fileptr); // Read in the entire file
+	FILE *filePtr = getFilePtr(argv[1]);
+	long fileLength = getFileLength(filePtr);
+	int binaryArray[fileLength * 8];
 
-	int binaryArray[filelen * 8];
+	createBinaryArray(filePtr, fileLength, binaryArray);
+
+
+
+
+
+
+	int newArray[fileLength * 8 * 2];
+	int lastIndex = compressArray(binaryArray, newArray, fileLength * 8);
+
+//	printBinArray(newArray, lastIndex - 1);
+
+	newBuffer = (char *)malloc((lastIndex/4 + 1) * sizeof(char));
+
+	writeptr = fopen("new.out", "wb");
+	fwrite(newBuffer, sizeof(newBuffer), 1, writeptr);
+
+	fclose(writeptr);
+}
+
+FILE *getFilePtr(char *fileName){
+	FILE *filePtr;
+	filePtr = fopen(fileName, "rb"); 	 // Open the file in binary mode
+	
+	return filePtr;
+}
+
+long getFileLength(FILE *filePtr){
+	long fileLength;
+
+	fseek(filePtr, 0, SEEK_END);          // Jump to the end of the file
+	fileLength = ftell(filePtr);          // Get the current byte offset in the file
+	rewind(filePtr);                      // Jump back to the beginning of the file
+
+	return fileLength;
+}
+
+void createBinaryArray(FILE *filePtr, long fileLength, int binaryArray[]){
+	char *buffer;
+	buffer = (char *) malloc((fileLength + 1) *sizeof(char)); 
+
+	fread(buffer, fileLength, 1, filePtr); // Read in the entire file
 
 	int i = 0;
-	for (; i < filelen; i++){
+	for (; i < fileLength; i++){
 		int tempArray[8];
 		charToBinary(tempArray, buffer[i]);
 
@@ -33,20 +74,8 @@ int main (int argc, char **argv) {
 		}
 	}
 
-//	printBinArray(binaryArray, filelen * 8);
-
-	int newArray[filelen * 8 * 2];
-	int lastIndex = compressArray(binaryArray, newArray, filelen * 8);
-
-//	printBinArray(newArray, lastIndex - 1);
-
-	newBuffer = (char *)malloc((lastIndex/4 + 1) * sizeof(char));
-
-	writeptr = fopen("new.out", "wb");
-	fwrite(newBuffer, sizeof(newBuffer), 1, writeptr);
-
-	fclose(fileptr); // Close the file
-	fclose(writeptr);
+	printBinArray(binaryArray, fileLength * 8);
+	fclose(filePtr); 
 }
 
 
@@ -58,6 +87,10 @@ void charToBinary (int arr[], char ch){
 		arr[7 - i] = num & 0x1;
 		num = num >> 1;
 	}
+}
+
+void binaryToChar(int arr[], int count, int bit){
+
 }
 
 
